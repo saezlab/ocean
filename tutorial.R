7efd5ffab129dc3e9b86161c97494e1a740aeb8b
adugourd <adugourd@ukaachen.de>
2020-12-15 19:04:16 +0100

    added imports to description

--- tutorial.R
+++ tutorial.R
@@ -21,9 +21,9 @@ t_table <- t_table_metactivity_input_formater(metabolomic_t_table = t_table,
 penalty_min <- 6 #minimum 1 and integer
 penalty_max <- 6 #maximum 9 and integer
 
-reaction_set_list <- prepare_metabolite_set(penalty_range = penalty_min:penalty_max, 
+reaction_set_list <- prepare_metabolite_set(penalty_range = penalty_min:penalty_max,  
                                             forest = tree_without_cofactors,
-                                            metab_list = t_table$KEGG)
+                                            measured_metabolites = t_table$KEGG)
 
 reaction_set_list_merged <- condense_metabolite_set(reaction_set_list = reaction_set_list)
 
@@ -45,7 +45,12 @@ mean_NES_df <- metactivity_res$NES
 translated_results <- translate_results(regulons_df = regulons_df, t_table = t_table, mapping_table = mapping_table)
 
 ##Visualise results for single enzmes
-plots <- plotMetaboliteContribution(enzyme = 'FH', stat_df = translated_results$t_table, metabolite_sets = translated_results$regulons_df, contrast_index = 1, stat_name = 't', scaling_factor = 5)
+plots <- plotMetaboliteContribution(enzyme = 'SUCLA2', 
+                                    stat_df = translated_results$t_table, 
+                                    metabolite_sets = translated_results$regulons_df, 
+                                    contrast_index = 1, 
+                                    stat_name = 't', 
+                                    scaling_factor = 15)
 
 plot(plots$scatter)
 plot(plots$cumsumPlot)
