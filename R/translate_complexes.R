9b766a7400b26304f5dad75842d882b377322c33
Caroline2501 <c.lohoff97@gmx.de>
2021-06-09 23:41:30 +0200

    Added one function to translate_complexes.R and updated tutorial_submodules.R

--- R/translate_complexes.R
+++ R/translate_complexes.R
@@ -19,7 +19,7 @@
 library(ocean)
 library(dplyr)
 library(magrittr)
-library(sjmisc)   # function "str_contains()"
+library(sjmisc)   
 
 
 #'\code{translate_complexes}
@@ -28,47 +28,46 @@ library(sjmisc)   # function "str_contains()"
 #'identifiers to their enzyme name using recon2_redhuman, and give back the full 
 #'source/target network with enzymes, metabolites and translated complexes.
 #'
-#'@param  cellNetwork source/target network containing enzymes, complexes, metabolites
+#'@param Network source/target network containing enzymes, complexes, metabolites
 #'@return source/target network containing enzymes, translated complexes, metabolites
 
-translate_complexes <- function(cellNetwork){
+translate_complexes <- function(Network){
   
+  ##Add new column to network data frame for later comparisons
+  Network$row_names <- 1:nrow(Network) 
+  Network <- Network[c("row_names", "source", "target")]
   
-  # add new column to cell_network df for later comparisons
-  cell_network$row_names <- 1:nrow(cell_network) 
-  cell_network <- cell_network[c("row_names", "source", "target")]
+  ##Select all strings that start with 3 digits "^\d{3}" (=complexes)
+  network_source <- Network[grep("^\\d{3}", Network$source), ]
+  network_target <- Network[grep("^\\d{3}", Network$target), ]  
   
+  ##Delete all rows with complexes from network dataframe
+  network_minus <- anti_join(Network, network_source, by= "row_names")
+  network_minus <- anti_join(network_minus, network_target, by= "row_names")  
   
-  # select all strings that start with 3 digits "^\d{3}" (=complexes)
-  # and delete those rows from cell_network dataframe
-  cell_network_source <- cell_network[grep("^\\d{3}", cell_network$source), ]
-  cell_network_minus <- anti_join(cell_network, cell_network_source, by= "row_names")
-  # split enzymes in complexes to separate strings using underscore as splitting condition
-  for(i in 1:length(cell_network_source$source)){
-    cell_network_source$source[i] <- strsplit(as.character(cell_network_source$source[i]), split = "_")
+  ##Split complexes to separated enzymes using underscore as splitting condition
+  split_complexes <- function(Column){
+    for(i in 1:length(Column)){
+      Column[i] <- strsplit(as.character(Column[i]), split = "_")
+    }
+    return(Column)
   }
+  network_source$source <- split_complexes(network_source$source)
+  network_target$target <- split_complexes(network_target$target)
   
-  cell_network_target <- cell_network[grep("^\\d{3}", cell_network$target), ]
-  cell_network_minus <- anti_join(cell_network_minus, cell_network_target, by= "row_names")
-  for(i in 1:length(cell_network_target$target)){
-    cell_network_target$target[i] <- strsplit(as.character(cell_network_target$target[i]), split = "_")
-  }
+  ##Combine source and target dataframes
+  network_complex <- rbind(network_source, network_target)
   
-  # combine both dataframes
-  cell_network_complex <- rbind(cell_network_source, cell_network_target)
-  
-  # 3. use recon2_redhuman$gene_mapping and map complexes to identifiers
+  ##Use recon2_redhuman$gene_mapping and map enzyme names to identifiers
   mapping_vec <- recon2_redhuman$gene_mapping$name
   names(mapping_vec) <- recon2_redhuman$gene_mapping$X1
   
-  a <- cell_network_complex
-  
-  for (i in 2:ncol(a))   # columns 2 and 3 of df
+  for (i in 2:ncol(network_complex))      #iterate through columns 2 and 3 of df
   {
-    for (j in 1:length(a[,2])) # j goes through rows of df
+    for (j in 1:length(network_complex[,2]))      #j iterates through rows of df
     {
       str <- ""
-      for (entry in a[j,i][[1]])
+      for (entry in network_complex[j,i][[1]])
       {
         if(entry %in% names(mapping_vec))
         {
@@ -77,34 +76,32 @@ translate_complexes <- function(cellNetwork){
           str <- paste(str, as.character(entry),sep="_")
         }
         
-        # Take the > into account
+        ##Take the ">" into account
         if(str_contains(entry,">"))
         {
-          print(entry)
           str <- gsub(entry, ">", str)
           str <- substr(str, 1, nchar(str)-3)
           pair <- strsplit(entry, split=">")
           
-          value1 <- mapping_vec[[pair[[1]][1]]]
-          value2 <- pair[[1]][2]
-          
-          tmp <- paste(value1,">",value2,sep="")
+          tmp <- paste(mapping_vec[[pair[[1]][1]]],">",pair[[1]][2],sep="")
           str <- paste(str, tmp,sep="_")
         }
       }
-      a[j,i][[1]] <- substring(str,2)
+      network_complex[j,i][[1]] <- substring(str,2)
     }
   }
   
-  # add rows with translated complexes to cell_network_minus
-  cell_network_translated <- rbind(cell_network_minus, a)
-  # order rows by column "row_names"
-  cell_network_translated <- cell_network_translated[order(cell_network_translated[,1]), ]
-  # delete column "row_names"
-  cell_network_translated$row_names <- NULL
-  # convert all columns to characters in dataframe
-  cell_network_translated <- cell_network_translated %>%
-                              mutate(across(everything(), as.character))
+  ##Add rows with translated complexes to network from beginning & order rows
+  network_translated <- rbind(network_minus, network_complex)
+  network_translated <- network_translated[order(network_translated[,1]), ]
+  network_translated$row_names <- NULL
+ 
+  ##To have the rows of the new order listed sequentially
+  rownames(network_translated) <- 1:nrow(network_translated)
   
-  return(cell_network_translated)
+  ##Convert all columns to characters
+  network_translated <- network_translated %>%
+                              mutate(across(everything(), as.character))
+
+  return(network_translated)
 }
--- tutorial_submodules.R
+++ tutorial_submodules.R
@@ -18,7 +18,7 @@ library(ocean)
 source("./R/translate_complexes.R")
 
 
-## 1. Differential analysis
+##Differential analysis
 unique(toy_targets$condition)
 comparisons <- list('tumorVsHealthy' = c(1,-2))
 
@@ -36,27 +36,19 @@ t_table <- ttop_list_to_t_table(
 t_table <- t_table_metactivity_input_formater(metabolomic_t_table = t_table,
                                               mapping_table = mapping_table,
                                               affixes = c("c","l","x","m","e","n","r"))
-## 2. Prepare input data
 
 ##Prepare the metabolic enzyme sets
 penalty_min <- 6 #minimum 1 and integer
 penalty_max <- 6 #maximum 9 and integer
 
-## These are the available pathways to choose from
+##These are the available pathways to choose from
 View(unique(recon2_redhuman$pathway))
 
-##Select all pathways from recon2_redhuman$pathway to be included in network
-all_pathways <- as.vector(unique(recon2_redhuman$pathway))
-all_pathways <- all_pathways[!is.na(all_pathways)]#remove row with NA as pathway
-
-##create network with metabolites/enzymes as sources/targets
-TCA_network <- model_to_pathway_sif(pathway_to_keep = all_pathways)
-
-# alternative: select only interesting pathways
-#cell_network <- model_to_pathway_sif(pathway_to_keep = c("Citric acid cycle",
-#                                                        "Glycolysis/gluconeogenesis",
-#                                                        "Pyruvate metabolism",
-#                                                        "Transport, mitochondrial"))
+##Select pathways relevant to include in network
+TCA_network <- model_to_pathway_sif(pathway_to_keep = c("Citric acid cycle",
+                                                        "Glycolysis/gluconeogenesis",
+                                                        "Pyruvate metabolism",
+                                                        "Transport, mitochondrial"))
 
 ##Translate enzyme complexes by mapping identifiers to names
 TCA_network_trans <- translate_complexes(TCA_network)
@@ -76,7 +68,7 @@ enzymes <- enzymes[!grepl("_[clxmenr]$",enzymes)]
 
 #branch_length applies a cutoff on the minimum length of the reaction network 
 #upstream and down stream of a given enzyme. Here we use a minimum length of 3
-#for both directions
+#for both directions.
 TCA_forest <- forestMaker(enzymes, TCA_network_nocofact$reaction_network,
                           branch_length = c(3,3))
 
@@ -90,14 +82,13 @@ penalty <- 6 #has to be between penalty_min and penalty_max and integer
 
 regulons_df <- prepare_regulon_df(reaction_set_list_merged, penalty, c(0,1))
 
-## 3. Compute metabolic enzme enrichment score
+##Compute metabolic enzyme enrichment score
 metactivity_res <- metactivity(metabolomic_t_table = t_table, 
                                regulons_df = regulons_df, 
                                compartment_pattern = "_[a-z]$", 
                                k = 1000)
 
 mean_ES_df <- metactivity_res$ES
-
 mean_NES_df <- metactivity_res$NES
 
 ##translate the metabolic ids back to names
@@ -105,9 +96,8 @@ translated_results <- translate_results(regulons_df = regulons_df,
                                         t_table = t_table,
                                         mapping_table = mapping_table)
 
-## 4. Visualisation
-##Visualise results for single enzmes
-plots <- plotMetaboliteContribution(enzyme = '4967_1738_8050_1743', 
+##Visualise results for single enzymes
+plots <- plotMetaboliteContribution(enzyme = 'OGDH_DLD_PDHX_DLST', 
                                     stat_df = translated_results$t_table, 
                                     metabolite_sets = translated_results$regulons_df, 
                                     contrast_index = 1, 
@@ -120,12 +110,8 @@ plot(plots$cumsumPlot)
 
 ##Visualise results at KEGG pathway level
 hm <- pathway_HM(mean_NES_df = mean_NES_df, pathway_name = 'KEGG_CITRATE_CYCLE_TCA_CYCLE', pathways = kegg_pathways)
-#hm <- pathway_HM(mean_NES_df = mean_NES_df, pathway_name = 'CELL_METABOLISM', pathways = kegg_pathways)
 plot(hm)
 
-## visualise the network
+##Visualise the network
 plot_reaction_network(TCA_network_nocofact, t_table, mean_NES_df,
                       column_index = 1, vis.height = 2000)
-
-## 5. Create new dataframe for getting pathway infos for metabolites
-metab_pathways_df <- map_pathways_to_metabolites(translated_results$regulons_df)
